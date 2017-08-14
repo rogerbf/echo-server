23032301fb14252f15cdc68b1ba86ffee0c53657
rogerbf <roger@bergfalk.nu>
2017-08-14 19:09:01 +0200

    refactor

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,22 @@
-import { main as cjs, module as esm, dependencies } from './package.json'
+import pkg from './package.json'
 import babel from 'rollup-plugin-babel'
 
 export default Object.assign(
   {
     entry: `source/main.js`,
     targets: [
-      { dest: cjs, format: `cjs` },
-      { dest: esm, format: `es` },
+      { dest: pkg.main, format: `cjs` },
+      { dest: pkg.module, format: `es` }
     ],
     plugins: [
       babel({ exclude: `node_modules/**` })
     ]
   },
-  dependencies ? { external: Object.keys(dependencies) } : {},
+  {
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      `http`,
+      `net`
+    ]
+  }
 )
